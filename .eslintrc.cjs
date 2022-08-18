56dbf829bd0f63009ae3135816ebcb25fee02412
pan93412 <pan93412@gmail.com>
2022-08-18 19:00:21 +0800

    Add A11y rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,13 @@
 module.exports = {
 	extends: [
+		"plugin:jsx-a11y/recommended",
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:solid/typescript",
 		"prettier",
 	],
 	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint", "solid"],
+	plugins: ["@typescript-eslint", "solid", "jsx-a11y"],
 	root: true,
 	rules: {
 		"@typescript-eslint/consistent-type-imports": [
@@ -15,5 +16,12 @@ module.exports = {
 				prefer: "type-imports",
 			},
 		],
+	},
+	settings: {
+		"jsx-a11y": {
+			components: {
+				Button: "button",
+			},
+		},
 	},
 };
