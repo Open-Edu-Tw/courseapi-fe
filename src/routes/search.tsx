2aa3b370abced3362d1bcabf32519f52877c528d
pan93412 <pan93412@gmail.com>
2022-09-01 20:44:23 +0800

    fix(search): Use gray-800 for light theme

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -27,7 +27,7 @@ export default function Search() {
 	const [searchResult] = createResource(query, searchApi);
 
 	return (
-		<main class="text-gray-200 px-40">
+		<main class="text-gray-800 dark:text-gray-200 px-40">
 			<Title>CourseAPI – 「{query()}」的搜尋結果</Title>
 
 			<h1 class="font-bold text-4xl mb-4">「{query()}」的搜尋結果</h1>
